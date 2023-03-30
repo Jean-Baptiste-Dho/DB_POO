836607d2bcee7f6ab32a700cf3b6f83ee18204b6
Jean-Baptiste <jeanbaptiste.dho@gmail.com>
2023-03-30 16:45:37 +0200

    mise en commentaire des lignes de test dans index.php

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php
 
 use App\Autoloader;
-use App\Models\AnnoncesModel;
+//use App\Models\AnnoncesModel;
 
 require_once 'Classes/Autoloader.php';
 Autoloader::register();
 
 
-$model = new AnnoncesModel;
+// $model = new AnnoncesModel;
 
-$données = [
-    'titre' => 'Annonce hydratée',
-    'desciption' => 'Description de l\'annonce hydratée',
-    'actif' => 0,
-];
+// $données = [
+//     'titre' => 'Annonce hydratée',
+//     'desciption' => 'Description de l\'annonce hydratée',
+//     'actif' => 0,
+// ];
 
-$annonce = $model->hydrate($données);
+// $annonce = $model->hydrate($données);
 
-$model->update(2, $annonce);
+// $model->update(2, $annonce);
